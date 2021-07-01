07af4004aa7717f50b96ee4d95161a8cd6d57d18
almazbekovbeks99 <almazbekovbeks99@gmail.com>
2021-07-01 20:49:46 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,79 @@
+// let a = +prompt('num?')
+
+// let arr = []
+
+// let newArr = []
+
+// const num = () => {
+//     for(let i = 0; i < a; i++){
+//         let ask = prompt('name?')
+//         arr[i] = ask
+//     }
+    
+//     let b = prompt('name?')
+
+//     for(let i = 0; i < a; i++){
+//         if(b === arr[i]){
+//             newArr[i] = b
+//         }
+//     }
+    
+//     let str = b.split("").reverse().join("")
+
+//     console.log(str);
+// }
+
+// num()
+
+// console.log(arr);
+
+// console.log(newArr);
+
+
+
+
+// let arr = ['beksultan' , 'munarbek' , 'diana' , true , false , 1 ,2 ,45 , 'almaz' , 'bael']
+
+// let newArr = []
+
+// const num = () => {
+//     for(let i = 0; i < arr.length; i++){
+//         if(typeof arr[i] === 'string'){
+//             if(arr[i].length > 5){
+//                 newArr.push(arr[i])
+//             }else if(arr[i].length <= 5){
+//                 arr[i]= 0
+//             }
+//         }
+//     }
+// }
+
+// num()
+
+// console.log(arr);
+
+// console.log(newArr);
+
+// console.log(newArr.join(", "));
+
+
+
+
+
+
+
+// var a = +prompt('num?')
+
+// function num(item){
+//     return -Math.abs(item)
+// }
+// console.log(num(a));
+
+
+// const num = item => {
+//     return -Math.abc(item)
+// }
+
+// console.log(num(a));
+
+
